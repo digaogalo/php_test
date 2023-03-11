9ac75cac0b22dc2ed81fe83fa52594cc1f6e86b9
digaogalo <digaolimac@gmail.com>
2023-03-11 10:43:54 -0300

    php form GET

--- 10_get_post.php
+++ 10_get_post.php
@@ -4,12 +4,25 @@
 /*
   We can pass data through urls and forms using the $_GET and $_POST superglobals.
 */
+
+echo $_GET['name'];
+echo $_GET['age'];
 ?>
 
-<a href="   
-<?php echo $_SERVER['PHP_SELF'];    
-?>
-?name="
->
-</a>
-   
+<a href="<?php $_SERVER['PHP_SELF']; ?>?
+name=John&age=30">Click</a>
+
+<form action="<?php $_SERVER['PHP_SELF']; ?>"
+method="GET">
+  <div>
+    <label for="name">Name: </label>
+    <input type="text" name="name">
+  </div>
+  <div>
+    <label for="age">Age: </label>
+    <input type="text" name="age">
+  </div>
+  <input type="submit" value="Submit" name="submit">
+</form>
+
+
